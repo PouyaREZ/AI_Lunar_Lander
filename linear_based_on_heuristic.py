7be870458d067cf7a3d2b3ca13a6bc25c2c92e88
Pouya Rezazadeh <rezazadeh.pouya@gmail.com>
2019-12-21 01:48:18 -0800

    Update linear_based_on_heuristic.py

--- linear_based_on_heuristic.py
+++ linear_based_on_heuristic.py
@@ -1,3 +1,11 @@
+'''###########################################
+CS221 Final Project: Heuristic Linear Q-Learning Implementation
+Authors:
+Kongphop Wongpattananukul (kongw@stanford.edu)
+Pouya Rezazadeh Kalehbasti (pouyar@stanford.edu)
+Dong Hee Song (dhsong@stanford.edu)
+###########################################'''
+
 import sys, math
 import numpy as np
 from collections import defaultdict
